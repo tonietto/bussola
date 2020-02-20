dd7b35df853a70179b29a206cc237a30c2ef6929
Gustavo Tonietto <tonietto@gmail.com>
2020-02-20 22:13:52 +0100

    Add small improvements

--- _javascript/main.js
+++ _javascript/main.js
@@ -6,112 +6,32 @@ import Viz from "viz.js";
 import { Module, render } from "viz.js/full.render.js";
 
 import { dummyData } from "./dummyData";
+import { AllElements } from "./AllElements";
 
 const editor = ace.edit("editor");
 editor.getSession().setMode("ace/mode/yaml");
 editor.setTheme("ace/theme/github");
 editor.setValue(dummyData);
 
-class AllElements {
-  constructor(params) {
-    this.units = params.units;
-    this.defaultFilters = params.defaultFilters;
-  }
-  availableFilters() {
-    let filters = {};
-    this.availableDirectives().forEach(directive => {
-      let filtersForDirective = {};
-      this.units.forEach(
-        unit => (filtersForDirective[unit.metadata[directive]] = true)
-      );
-      filters[directive] = Object.keys(filtersForDirective);
-    });
-    return filters;
-  }
-  availableDirectives() {
-    let allDirectives = {};
-    this.units.forEach(unit =>
-      Object.keys(unit.metadata).forEach(k => (allDirectives[k] = true))
-    );
-    return Object.keys(allDirectives);
-  }
-  filteredGraph(filter, directives) {
-    let dotOutput = "digraph G {\n";
-    const filteredUnits = this.getFilteredUnits(filter);
-    dotOutput += this.plotUnits(filteredUnits, directives);
-    dotOutput += this.resolveConnections(filteredUnits);
-    dotOutput += "}";
-    return dotOutput;
-  }
-  getFilteredUnits() {
-    // TODO
-    return this.units;
-  }
-  plotUnits(filteredUnits, directives) {
-    if (directives.length) {
-      let currentDirective = directives.shift();
-      let groups = groupUnitsBy(filteredUnits, currentDirective);
-      let output = "";
-      Object.keys(groups).forEach(groupName => {
-        const unitsOfGroup = groups[groupName];
-        if (groupName) {
-          output += `subgraph cluster_${currentDirective}_${groupName} {\n`;
-          output += `href = "${currentDirective}___${groupName}";\n`;
-          output += `label = "${groupName}";\n`;
-          output += this.plotUnits(unitsOfGroup, directives);
-          output += "}\n";
-        } else {
-          output += this.plotUnits(unitsOfGroup, directives);
-        }
-      });
-      return output;
-    } else {
-      let output = "";
-      filteredUnits.forEach(unit => {
-        output += `${unit.name} [label="${unit.name}"]; \n`;
-      });
-      return output;
-    }
-  }
-  resolveConnections(filteredUnits) {
-    const names = filteredUnits.map(u => u.name);
-    let output = "";
-    filteredUnits.forEach(unit =>
-      (unit.dependsOn || []).forEach(dependency => {
-        if (names.indexOf(dependency) !== -1) {
-          output += `${unit.name} -> ${dependency};`;
-        }
-      })
-    );
-    return output;
-  }
-}
-
-const groupUnitsBy = (units, directive) =>
-  units.reduce(
-    (result, unit) => ({
-      ...result,
-      [unit.metadata[directive] || ""]: [
-        ...(result[unit.metadata[directive] || ""] || []),
-        unit
-      ]
-    }),
-    {}
-  );
-
-document.getElementById("render").onclick = e => {
-  e.preventDefault();
+document.getElementById("render").onclick = () => {
   const parsedYAML = YAML.parse(editor.getValue());
-  window.allElements = new AllElements(parsedYAML);
-  window.output = window.allElements.filteredGraph(
+
+  const allElements = new AllElements(parsedYAML);
+
+  /*
+   * Append all elements to the window object
+   */
+  window.allElements = allElements;
+
+  const output = allElements.filteredGraph(
     parsedYAML.filter || {},
     parsedYAML.directives || []
   );
 
-  new Viz({ Module, render }).renderSVGElement(window.output).then(element => {
-    window.graphElement = document.getElementById("graph");
-    window.graphElement.innerHTML = "";
-    window.graphElement.appendChild(element);
+  new Viz({ Module, render }).renderSVGElement(output).then(element => {
+    const graphElement = document.getElementById("graph");
+    graphElement.innerHTML = "";
+    graphElement.appendChild(element);
   });
 };
 
